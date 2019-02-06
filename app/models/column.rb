fef2093b57b3bfcdf2796d3bff61314f43ed51dc
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-02-06 14:35:51 +0000

    application.htmlを編集

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,7 +1,7 @@
 class Column < ApplicationRecord
     def change
     create_table :columns do |t|
-      t.string      :name
+      t.text        :name
       t.text        :text
       t.timestamps null: true
     end
