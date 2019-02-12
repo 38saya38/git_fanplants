5948dd6c3e9f9fa295fea6b056629d33e43c5bcc
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-02-12 05:53:35 +0000

    modelの下にscraping用ファイルを作成

--- scraping_image.rb
+++ scraping_image.rb
@@ -2,5 +2,7 @@ require 'mechanize'
 
 agent = Mechanize.new
 page = agent.get("https://www.pakutaso.com/20160622154post-8028.html")
-elements = page.search('.photoEntry__image img')
-puts elements.get_attribute('src')
+elements = page.search('.photoEntry__thumb img')
+elements.each do |ele|
+  puts ele.get_attribute('src') # get_attributeメソッドで属性srcの値を取得
+end
