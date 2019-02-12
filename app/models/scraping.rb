044cf9c68531768bcdad26207614125296775e50
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-02-12 10:23:00 +0000

    スクレイピング 用のファイルを作成

---
+++ app/models/scraping.rb
@@ -0,0 +1,20 @@
+class Scraping
+   def self.scraping_image
+     links = []
+     agent = Mechanize.new
+     page = agent.get("https://www.pakutaso.com/20160622154post-8028.html")
+     elements = page.search('.photoEntry__thumb img')
+      elements.each do |ele|
+       links << ele.get_attribute('src') 
+      end
+   end
+   
+   def self.scraping_save(link)
+       agent = Mechanize.new
+       page = agent.get(link)
+       image = page.at('.photoEntry__thumb img')[:src]if page.at('photoEntry__thumb img')
+       
+       column = Column.new(image:image)
+       column.save
+   end
+end
--- scraping_image.rb
+++
@@ -1,8 +0,0 @@
-require 'mechanize'
-
-agent = Mechanize.new
-page = agent.get("https://www.pakutaso.com/20160622154post-8028.html")
-elements = page.search('.photoEntry__thumb img')
-elements.each do |ele|
-  puts ele.get_attribute('src') # get_attributeメソッドで属性srcの値を取得
-end
