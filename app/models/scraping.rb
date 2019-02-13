4782a8932b581c232cd4f11f60d81ffc1b712ea4
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-02-13 02:14:54 +0000

    migrationファイルを変更

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,19 +1,18 @@
 class Scraping
    def self.scraping_image
-     links = []
      agent = Mechanize.new
      page = agent.get("https://www.pakutaso.com/20160622154post-8028.html")
      elements = page.search('.photoEntry__thumb img')
-      elements.each do |ele|
-       links << ele.get_attribute('src') 
-      end
-   end 
+       elements.each do |ele|
+         puts ele.get_attribute('src')
+       end
+   end
     
    
-   def self.scraping_save(links)
+   def self.scraping_save
        agent = Mechanize.new
-       page = agent.get(links)
-       image = page.at('.photoEntry__thumb img')[:src] if page.at('photoEntry__thumb img')
+       page = agent.get(link)
+       image = page.at('.photoEntry__thumb img')[:src] 
        
        column = Column.new(image:image)
        column.save
--- db/migrate/20190206123804_create_columns.rb
+++ db/migrate/20190206123804_create_columns.rb
@@ -1,7 +1,7 @@
 class CreateColumns < ActiveRecord::Migration[5.2]
   def change
     create_table :columns do |t|
-      t.text       :name
+      t.text        :title
       t.text        :image
       t.timestamps null: true
      end
