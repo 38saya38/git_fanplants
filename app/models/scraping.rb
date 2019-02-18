a8a4e7085b0e125da9caa95994041dbab8e79c8a
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-02-18 05:29:41 +0000

    スクレイピングした画像をデータベースに保存できるようにする

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -4,12 +4,17 @@ class Scraping
      page = agent.get("https://www.pakutaso.com/20160622154post-8028.html")
      elements = page.search('.photoEntry__thumb img')
        elements.each do |ele|
+         column = Column.new(image: ele.get_attribute("src"),
+                             title: ele.get_attribute("alt"))
+         column.save
+         p column
          puts ele.get_attribute('src')
        end
    end
     
    
    def self.scraping_save
+    #   link = "https://www.pakutaso.com/20160622154post-8028.html"
        agent = Mechanize.new
        page = agent.get(link)
        image = page.at('.photoEntry__thumb img')[:src] 
