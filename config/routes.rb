00f4ffff458e47e3a36796237c9c6011da7c3d4c
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-03-11 02:04:39 +0000

    thanksルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'tops#index'
   get 'tops/contact' =>'tops#contact'
   post 'tops/confirm' =>'tops#confirm'
+  post 'tops/thanks' =>'tops#thanks'
   get 'tops/about'   =>'tops#about'
   get 'tops/link'    =>'tops#link'
   get 'pages/elements'=>'pages#elements'
