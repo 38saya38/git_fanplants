b6bfae334c94f8215c215f0dfbe366b5d7c592b2
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-03-11 02:08:24 +0000

    問い合わせフォーム作成の説明を記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'tops#index'
-  get 'tops/contact' =>'tops#contact'
-  post 'tops/confirm' =>'tops#confirm'
-  post 'tops/thanks' =>'tops#thanks'
+  get 'tops/contact' =>'tops#contact' #問い合わせフォーム
+  post 'tops/confirm' =>'tops#confirm' #確認画面
+  post 'tops/thanks' =>'tops#thanks' #送信完了画面
   get 'tops/about'   =>'tops#about'
   get 'tops/link'    =>'tops#link'
   get 'pages/elements'=>'pages#elements'
