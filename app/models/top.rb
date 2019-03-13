1bb544bf564f6784c2797744a5248394126670a8
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-03-13 12:48:21 +0000

    topmodelを編集

--- app/models/top.rb
+++ app/models/top.rb
@@ -1,2 +1,10 @@
 class Top < ApplicationRecord
+    class Top
+  include ActiveModel::Model
+ 
+  attr_accessor :name, :email, :message
+ 
+  validates :name, :presence => {:message => '名前を入力してください'}
+  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+    end
 end
