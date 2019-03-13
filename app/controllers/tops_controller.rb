37ad6f9da82e8e54ca887bcd8d19d605514b72d4
EC2 Default User <ec2-user@ip-172-31-1-54.ap-southeast-1.compute.internal>
2019-03-13 12:26:06 +0000

    confirmアクションを定義

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -8,6 +8,15 @@ class TopsController < ApplicationController
     end
     
     def confirm
+         # 入力値のチェック
+      @top = Top.new(params[:top])
+      if @top.valid?
+      # OK。確認画面を表示
+      render :action => 'confirm'
+      else
+      # NG。入力画面を再表示
+      render :action => 'index'
+      end
     end
     
     def thanks
